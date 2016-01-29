ee38480212c9078a3381ff0c9ebd54cca45421a6
Yoshito Tabuchi on Y510p <ytabuchi.xlsoft@gmail.com>
2016-01-29 18:29:33 +0900

    1st commit

---
+++ XF_Geolocator/XF_Geolocator/XF_Geolocator/GetGeoCS.cs
@@ -0,0 +1,88 @@
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+
+using Xamarin.Forms;
+
+namespace XF_Geolocator
+{
+    public class GetGeoCS : ContentPage
+    {
+        Button button;
+        Label latLabel;
+        Label lonLabel;
+        Plugin.Geolocator.Abstractions.Position position;
+
+        public GetGeoCS()
+        {
+            button = new Button
+            {
+                Text = "Get Geolocator"
+            };
+            button.Clicked += async (sender, e) =>
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+
+                System.Diagnostics.Debug.WriteLine("Available: " + locator.IsGeolocationAvailable);
+                System.Diagnostics.Debug.WriteLine("Enable: " + locator.IsGeolocationEnabled);
+
+
+                if (locator.IsGeolocationAvailable)
+                {
+                    if (locator.IsGeolocationEnabled)
+                    {
+                        try
+                        {
+                            position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+                            latLabel.Text = "Latitude: " + position.Latitude;
+                            lonLabel.Text = "Longitude: " + position.Longitude;
+
+                            System.Diagnostics.Debug.WriteLine("TimeStamp: " + position.Timestamp);
+                        }
+                        catch (GeolocationException ex)
+                        {
+                            System.Diagnostics.Debug.WriteLine(ex.InnerException);
+                            await DisplayAlert("Error", "Unauthorized", "OK");
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Diagnostics.Debug.WriteLine(ex.InnerException);
+                        }
+                    }
+                    else
+                        await DisplayAlert("Error", "Geo sensors is not turned on.\nPlease turn on it.", "OK");
+
+                }
+                else
+                    await DisplayAlert("Error", "This device does not have any geo sensors", "OK");
+
+
+            };
+            latLabel = new Label
+            {
+                Text = "Latitude: "
+            };
+            lonLabel = new Label
+            {
+                Text = "Langitude: "
+            };
+
+            Content = new StackLayout
+            {
+                Padding = 10,
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    button,
+                    latLabel,
+                    lonLabel,
+                },
+            };
+        }
+    }
+}
---
+++ X_Geolocator/X_Geolocator/X_Geolocator.Droid/MainActivity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Locations;
+using Android.Util;
+
+namespace X_Geolocator.Droid
+{
+	[Activity (Label = "X_Geolocator.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+        LocationManager locMgr;
+        string tag = "MainActivity";
+        Button button;
+        TextView latText;
+        TextView lanText;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+
+			button = FindViewById<Button> (Resource.Id.myButton);
+            latText = FindViewById<TextView>(Resource.Id.LatText);
+            lanText = FindViewById<TextView>(Resource.Id.LanText);
+
+		}
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            // LocationManagerを初期化
+            locMgr = GetSystemService(Context.LocationService) as LocationManager;
+
+            button.Click += (sender, e) =>
+            {
+                if (locMgr.AllProviders.Contains(LocationManager.NetworkProvider)
+                        && locMgr.IsProviderEnabled(LocationManager.NetworkProvider))
+                {
+                    // Network: Wifi と 3G で位置情報を取得します。電池使用量は少ないですが精度にばらつきがあります。
+                    Log.Debug(tag, "Starting location updates with Network");
+                    locMgr.RequestLocationUpdates(LocationManager.NetworkProvider, 10000, 10, this);
+                }
+                else
+                {
+                    // GetBestProvider で最適な Provider を利用できるようです。(Network があればそれが Best になるようです。)
+                    var locationCriteria = new Criteria();
+                    locationCriteria.Accuracy = Accuracy.Coarse;
+                    locationCriteria.PowerRequirement = Power.Medium;
+                    string locationProvider = locMgr.GetBestProvider(locationCriteria, true);
+
+                    Log.Debug(tag, "Starting location updates with " + locationProvider.ToString());
+                    locMgr.RequestLocationUpdates(locationProvider, 10000, 10, this);
+                }
+            };
+        }
+    }
+}
+
+
