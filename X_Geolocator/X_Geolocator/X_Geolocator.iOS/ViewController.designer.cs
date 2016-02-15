eaa7ba8c0e64f3d1885b62a4dceaa8b94525a0bc
Yoshito Tabuchi on Y510p <ytabuchi.xlsoft@gmail.com>
2016-02-15 10:55:14 +0900

    Created iOS storyboard

--- X_Geolocator/X_Geolocator/X_Geolocator.iOS/ViewController.designer.cs
+++ X_Geolocator/X_Geolocator/X_Geolocator.iOS/ViewController.designer.cs
@@ -1,9 +1,11 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 using UIKit;
 
@@ -14,13 +16,29 @@ namespace X_Geolocator.iOS
 	{
 		[Outlet]
 		[GeneratedCode ("iOS Designer", "1.0")]
-		UIButton Button { get; set; }
-        
+		UIButton button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel LatText { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel LonText { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
-			if (Button != null) {
-				Button.Dispose ();
-				Button = null;
+			if (button != null) {
+				button.Dispose ();
+				button = null;
+			}
+			if (LatText != null) {
+				LatText.Dispose ();
+				LatText = null;
+			}
+			if (LonText != null) {
+				LonText.Dispose ();
+				LonText = null;
 			}
 		}
 	}
