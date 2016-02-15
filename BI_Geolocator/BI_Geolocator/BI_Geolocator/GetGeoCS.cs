6e02b2e26d1cd939ff520f8820d885b316d2a15b
Yoshito Tabuchi on x220 <ytabuchi.xlsoft@gmail.com>
2016-02-15 16:29:59 +0900

    Add Xamarin.Forms Project

---
+++ BI_Geolocator/BI_Geolocator/BI_Geolocator/GetGeoCS.cs
@@ -0,0 +1,83 @@
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BI_Geolocator
+{
+    public class GetGeoCS : ContentPage
+    {
+        Button button;
+        Label latLabel;
+        Label lonLabel;
+        Position position;
+
+        public GetGeoCS()
+        {
+            button = new Button
+            {
+                Text = "Get Geolocator"
+            };
+            button.Clicked += async (sender, e) =>
+            {
+                IGeolocator locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+
+                if (locator.IsGeolocationAvailable)
+                {
+                    if (locator.IsGeolocationEnabled)
+                    {
+                        try
+                        {
+                            position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+                            latLabel.Text = "Latitude: " + position.Latitude;
+                            lonLabel.Text = "Longitude: " + position.Longitude;
+
+                            System.Diagnostics.Debug.WriteLine("TimeStamp: " + position.Timestamp);
+                        }
+                        catch (GeolocationException ex)
+                        {
+                            System.Diagnostics.Debug.WriteLine(ex.InnerException);
+                            await DisplayAlert("Error", "Unauthorized", "OK");
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Diagnostics.Debug.WriteLine(ex.InnerException);
+                        }
+                    }
+                    else
+                        await DisplayAlert("Error", "Geo sensors is not turned on.\nPlease turn on it.", "OK");
+
+                }
+                else
+                    await DisplayAlert("Error", "This device does not have any geo sensors or enough permission to use it.", "OK");
+
+
+            };
+
+            latLabel = new Label
+            {
+                Text = "Latitude: "
+            };
+            lonLabel = new Label
+            {
+                Text = "Longitude: "
+            };
+
+            
+            Content = new StackLayout
+            {
+                Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 10),
+                Children = {
+                    button,
+                    latLabel,
+                    lonLabel
+                }
+            };
+        }
+    }
+}
---
+++ BI_Geolocator/BI_Geolocator/BI_Geolocator/GetGeoXaml.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BI_Geolocator
+{
+    public partial class GetGeoXaml : ContentPage
+    {
+        Position position;
+
+        public GetGeoXaml()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonClicked(object sender, EventArgs e)
+        {
+            IGeolocator locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            if (locator.IsGeolocationAvailable)
+            {
+                if (locator.IsGeolocationEnabled)
+                {
+                    try
+                    {
+                        position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+                        latLabel.Text = "Latitude: " + position.Latitude;
+                        lonLabel.Text = "Longitude: " + position.Longitude;
+
+                        System.Diagnostics.Debug.WriteLine("TimeStamp: " + position.Timestamp);
+                    }
+                    catch (GeolocationException ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex.InnerException);
+                        await DisplayAlert("Error", "Unauthorized", "OK");
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex.InnerException);
+                    }
+                }
+                else
+                    await DisplayAlert("Error", "Geo sensors is not turned on.\nPlease turn on it.", "OK");
+
+            }
+            else
+                await DisplayAlert("Error", "This device does not have any geo sensors or enough permission to use it.", "OK");
+        }
+    }
+}
